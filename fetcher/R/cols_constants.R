4b6dc18fe9ecc2977039a638bad3afbfde936258
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2020-06-02 19:20:59 -0300

    Realiza fetch da tabela de licitaçoes do sagres

---
+++ fetcher/R/cols_constants.R
@@ -0,0 +1,5 @@
+COLNAMES_LICITACOES <- c("cd_UGestora"="integer","dt_Ano"="integer","nu_Licitacao"="character","tp_Licitacao"="integer",
+                          "dt_Homologacao"="character","nu_Propostas"="integer","vl_Licitacao"="numeric",
+                          "tp_Objeto"="integer","de_Obs"="character","dt_MesAno"="character",
+                          "registroCGE"="character","tp_regimeExecucao"="integer"
+)
---
+++ fetcher/scripts/fetch_sagres_data.R
@@ -0,0 +1,24 @@
+library(magrittr)
+
+source(here::here("R/setup/constants.R"))
+source(here::here("R/sagres.R"))
+
+.HELP <- "Rscript fetch_sagres_data.R"
+
+sagres_2017 <- NULL
+  
+tryCatch({sagres_2017 <- DBI::dbConnect(RMySQL::MySQL(),
+                                dbname = MYSQL_DB,
+                                host = MYSQL_HOST,
+                                port = as.numeric(MYSQL_PORT),
+                                user = MYSQL_USER,
+                                password = MYSQL_PASSWORD)
+}, error = function(e) print(paste0("Erro ao tentar se conectar ao Banco Sagres (MySQL): ", e)))
+
+DBI::dbGetQuery(sagres_2017, "SET NAMES 'utf8'")
+
+licitacoes <- fetch_licitacoes(sagres_2017)
+
+readr::write_csv(licitacoes, here::here("./data/licitacoes.csv"))
+
+DBI::dbDisconnect(sagres_2017)
