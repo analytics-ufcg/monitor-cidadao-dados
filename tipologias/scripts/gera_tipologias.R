cc603e6b22c776322bf2dd8a43358cf2ead22496
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2020-08-07 12:57:57 -0300

    Gera zip com código fonte que cria as tipologias

--- tipologias/scripts/gera_tipologias.R
+++ tipologias/scripts/gera_tipologias.R
@@ -91,16 +91,25 @@ tipologias_final_contratos_gerais <- tipologias_merge %>%
   dplyr::left_join(tipologias_contrato, by = c("cd_u_gestora", "nu_contrato", "nu_cpfcnpj", "data_inicio")) %>% 
   dplyr::select(id_contrato, dplyr::everything())
 
+hash_source_code <- digest::digest(paste(Sys.time()), algo="md5", serialize=F)
+
 features <- tipologias_final_contratos_gerais %>% tidyr::gather(key = "nome_feature", 
                                          value = "valor_feature", 
                                          -id_contrato) %>% 
   dplyr::mutate(timestamp = Sys.time(),
-                hash_bases_geradoras = generate_hash_al_db(al_db_con))
+                hash_bases_geradoras = generate_hash_al_db(al_db_con),
+                hash_codigo_gerador_feature = hash_source_code)
 
-readr::write_csv(features, 
+readr::write_csv(tipologias_final_contratos_gerais, 
                  dplyr::if_else(vigentes,
                                 paste("data/tipologias_contratos_vigentes_", Sys.Date(), ".csv", sep = ""),
                                 paste("data/tipologias_contratos_gerais_", Sys.Date(), ".csv", sep = "")))
 
+readr::write_csv(features, 
+                 dplyr::if_else(vigentes,
+                                paste("data/features_vigentes_", Sys.Date(), ".csv", sep = ""),
+                                paste("data/features_gerais_", Sys.Date(), ".csv", sep = "")))
+
+zip(paste("data/source_code/",hash_source_code, ".zip", sep = ""), c("R", "scripts"))
 
 DBI::dbDisconnect(al_db_con)
