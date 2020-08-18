611e7260e963e91ba028f402d98bbbceac2f442c
Talita <talitabac@gmail.com>
2020-08-18 12:31:14 -0300

    Adiciona RF e CV na modelagem

--- tipologias/modelagem/treinamento_exc.R
+++ tipologias/modelagem/treinamento_exc.R
@@ -1,32 +1,19 @@
 # ---------------------------
-# Este script utiliza como entrada os arquivos:
-#
-# Para treinamento e avaliação dos modelos: 
-#   Floresta Aleatória e Regressão Logística
-#
-# Outputs desejados:
-#   indice_part.csv
-#   metricas.csv
-#   experimento.csv
-#     objeto_modelo?
-# ---------------------------
-
-
-
-
-# ---------------------------
 # File Help
 # ---------------------------
 
-
+#' @title Definição da mensagem de ajuda do script
 displayhelpmenu <- function() {
   # falta inserir as instruções sobre previsão nos contratos vigentes
-  cat("---------------------------------------Help treinamento_exc.R-----------------------------------------------------\n")
-  cat("Este script irá buscar pelas tipologias de contratos.\n")
+  cat("-----------------------------Help treinamento_exc.R--------------------------\n")
   cat("\n")
-  cat("Outputs desejados:\n   indice_part.csv \n   metricas.csv\n")
-  cat("---------------------------------------Help treinamento_exc.R-----------------------------------------------------\n")
-  
+  cat("Modelos treinados: Floresta Aleatória e Regressão Logística.\n")
+  cat("\n")
+  cat("Outputs desejados:\n")
+  cat("indice_exp_<data_geracao>.csv\n")
+  cat("metricas_<data_geracao>.csv\n")
+  cat("experimento_<data_geracao>.csv\n")
+  cat("-----------------------------Help treinamento_exc.R--------------------------\n")
 }
 
 
@@ -35,17 +22,15 @@ displayhelpmenu <- function() {
 # Recebimento de Parâmetros
 # ---------------------------
 
-if (length(args) == 0)
-  stop("Este script precisa de 1 parâmetros para execução. Execute --help.")
+args <- commandArgs(trailingOnly = TRUE)
 
 if (length(grep('--help', args))) {
   displayhelpmenu()
   quit()
 }
 
-if (length(args) < 5)
-  stop("Este script precisa de 1 parâmetros para execução. Execute --help.")
-
+# if (length(args) == 0)
+#   stop("Este script precisa de 1 parâmetros para execução. Execute --help.")
 
 
 
@@ -73,6 +58,35 @@ mygc <- function() invisible(gc())
 
 
 # ---------------------------
+# Funções
+# ---------------------------
+
+#' @title Gera hashcode do código fonte do treinamento
+generate_hash_sourcecode <- function() {
+  source_code_string <- paste(readr::read_file(here::here("modelagem/treinamento_exc.R")))
+  
+  hash_source_code <- digest::digest(source_code_string, algo="md5", serialize=F)
+}
+
+
+#' @title Gera identificador do experimento
+generate_id_experimento <- function(momentum) {
+  return(digest::digest(paste(momentum), algo="md5", serialize=F))
+}
+
+
+
+# ---------------------------
+# Variáveis Globais
+# ---------------------------
+
+data_hora <- gsub(":", "", gsub("[[:space:]]", "_", Sys.time()))
+id_experimento <- generate_id_experimento(data_hora)
+
+
+
+
+# ---------------------------
 # Parte I:
 #   Obtenção dos dados
 # ---------------------------
@@ -90,6 +104,7 @@ tipologias_cgerais <- tipologias_cgerais %>% filter_all(all_vars(!is.na(.)))
 
 tipologias_cgerais$status_tramita <- as.factor(tipologias_cgerais$status_tramita)
 tipologias_cgerais$tp_licitacao <- as.factor(tipologias_cgerais$tp_licitacao)
+
 
 
 
@@ -112,15 +127,18 @@ index <- createDataPartition(tipologias_cgerais$status_tramita, p = .8, list = F
 treino <- tipologias_cgerais[index,]
 teste  <- tipologias_cgerais[-index,]
 
+treino_feat <- treino %>% select(!features_exc)
+
+
+testinho <- data.frame(id_experimento = c(id_experimento),
+                       index_treino = as.list(index))
 
 index_treino_df <- as.data.frame(index) %>% mutate(indice_treino = Resample1) %>% select(-Resample1)
 
-readr::write_csv(index_treino_df, 
-                 paste("data/indice_part/indices_exp",
-                       "_",
-                       gsub(":", "", gsub("[[:space:]]", "_", Sys.Date())), ".csv", sep = ""))
 
-treino_feat <- treino %>% select(!features_exc)
+readr::write_csv(index_treino_df, 
+                 paste("data/indice_part/indices_exp", "_",
+                       gsub(":", "", gsub("[[:space:]]", "_", Sys.Date())), ".csv", sep = ""))
 
 
 rm(index_treino_df)
@@ -128,12 +146,13 @@ mygc()
 
 
 
+
 # ---------------------------
 # Parte III:
 #   Pré-processamento dos dados
-#   (alguns modelos têm melhor desempenho com dados escalados ou precisam de conversão factor-dummy manual)
 # ---------------------------
 
+# Dados escalados e conversão factor-dummy manual
 rl_receita <- recipe(status_tramita ~ ., data = treino_feat) %>% 
   step_scale(all_numeric(), -all_outcomes()) %>%
   step_dummy(all_nominal(), -all_outcomes()) %>% 
@@ -143,6 +162,14 @@ treino_assado <- juice(rl_receita)
 teste_assado <- bake(rl_receita, new_data = teste)
 
 
+# Balanceamento de classes
+
+# balance <- ubBalance(treino_exc, treino_exc$status_tramita, 
+#                      type = "ubSMOTE", positive = 1, k = 5, percOver = 1000, verbose = TRUE)
+# 
+# treino_upsample <- balance$X
+
+
 
 
 # ---------------------------
@@ -150,6 +177,7 @@ teste_assado <- bake(rl_receita, new_data = teste)
 #   Criação dos workflows para cada tipo de algoritmo
 # ---------------------------
 
+  # Regressão Logística
 reglog_mod <- logistic_reg() %>% set_engine("glm") %>% set_mode("classification") %>% translate()
 
 reglog_wf <- 
@@ -158,12 +186,15 @@ reglog_wf <-
   add_formula(status_tramita ~ .)
 
 
-# rf_mod <- rand_forest() %>% set_engine("randomForest") %>% set_mode("classification") %>% translate()
-# 
-# rf_wf <- 
-#   workflow() %>%
-#   add_model(rf_mod) %>%
-#   add_formula(status_tramita ~ .)
+  # Floresta Aleatória
+rf_mod <- rand_forest() %>% set_engine("randomForest") %>% set_mode("classification") %>% translate()
+
+rf_wf <-
+  workflow() %>%
+  add_model(rf_mod) %>%
+  add_formula(status_tramita ~ .)
+
+
 
 
 # ---------------------------
@@ -171,15 +202,34 @@ reglog_wf <-
 #   Validação cruzada 
 # ---------------------------
 
-cv_folds <- vfold_cv(treino_feat, v = 5)
+cv_folds <- vfold_cv(treino_assado, v = 5)
 
+  # Regressão Logística
 reglog_fit <- 
   reglog_wf %>% 
   fit_resamples(cv_folds,
                 metrics = metric_set(roc_auc, accuracy))
 
-reglog_metrics <- collect_metrics(reglog_fit) %>% mutate(model = "RegLog", balance = "none") 
+reglog_resmetrics <- collect_metrics(reglog_fit) %>% mutate(model = "Reg Logistica", balance = "none") 
 
+
+  # Floresta Aleatória
+rf_fit <- 
+  rf_wf %>% 
+  fit_resamples(cv_folds,
+                metrics = metric_set(roc_auc, accuracy))
+
+rf_resmetrics <- collect_metrics(rf_fit) %>% mutate(model = "Flo Aleatoria", balance = "none") 
+
+
+
+resample_metrics <- bind_rows(reglog_resmetrics, 
+                              rf_resmetrics)
+
+
+readr::write_csv(resample_metrics, 
+                 paste("data/metricas/resamples/cv_exp", "_",
+                       gsub(":", "", gsub("[[:space:]]", "_", Sys.Date())), ".csv", sep = ""))
 
 
 
@@ -188,7 +238,16 @@ reglog_metrics <- collect_metrics(reglog_fit) %>% mutate(model = "RegLog", balan
 #   Fit
 # ---------------------------
 
-reglog_fit <- reglog_wf %>% fit(treino_feat)
+
+  # Regressão Logística
+reglog_fit <- reglog_wf %>% fit(treino_assado)
+
+
+  # Floresta Aleatória
+rf_fit <- rf_wf %>% fit(treino_assado)
+
+
+
 
 
 # ---------------------------
@@ -196,16 +255,37 @@ reglog_fit <- reglog_wf %>% fit(treino_feat)
 #   Previsão do risco
 # ---------------------------
 
+# Separa apenas o gabarito
+ground_truth <- as.data.frame(teste$status_tramita)
+colnames(ground_truth) <- c("ground_truth")
 
-reglog_pred <- reglog_fit %>% predict(new_data = test_baked) %>% 
+
+  # Regressão Logística
+reglog_pred <- reglog_fit %>% predict(new_data = teste_assado) %>% 
   mutate(reglog_class_pred = .pred_class) %>% select(-.pred_class)
 
-reglog_probs <- reglog_fit %>% predict(new_data = test_baked, type = "prob") %>% 
+reglog_probs <- reglog_fit %>% predict(new_data = teste_assado, type = "prob") %>% 
   mutate(reglog_prob_0 = .pred_0, reglog_prob_1 = .pred_1) %>% select(-.pred_1, -.pred_0)
 
-teste <- teste %>% 
-  cbind(reglog_pred, reglog_probs) %>% 
-  mutate(reglog_class_pred = as.character(reglog_class_pred))
+
+
+  # Floresta Aleatória
+rf_class_pred <- rf_fit %>% predict(teste_assado) %>% 
+  mutate(rf_class_pred = .pred_class) %>% select(-.pred_class)
+
+rf_class_probs <- rf_fit %>% predict(teste_assado, type = "prob") %>% 
+  mutate(rf_prob_0 = .pred_0, rf_prob_1 = .pred_1) %>% select(-.pred_1, -.pred_0)
+
+
+previsoes <- bind_cols(ground_truth, 
+                       reglog_pred, 
+                       reglog_probs,
+                       rf_class_pred,
+                       rf_class_probs)
+
+
+rm(reglog_pred, reglog_probs, rf_class_pred, rf_class_probs)
+mygc()
 
 
 
@@ -215,40 +295,29 @@ teste <- teste %>%
 #   Calcula as métricas avaliativas para a previsão
 # ---------------------------
 
-av_reglog <- avaliacao("Reg Logística", teste, "reglog_class_pred", "status_tramita")
+
+av_reglog <- avaliacao("Reg Logistica", previsoes, "reglog_class_pred", "ground_truth")
+
+av_rf <- avaliacao("Flo Aleatoria", previsoes, "rf_class_pred", "ground_truth")
 
 
 
 
+avaliacao <- bind_rows(av_reglog,
+                       av_rf)
 
 
-
-# entrada:
-#   feature set
-
-
-# saídas:
-#   experimento
-#       id_experimento
-#       data
-#       objeto_modelo
-#       tipo_modelo
-#       modelo_hiperparams
-#       tipo_balanceamento
-#       fk_indice_part
-#       fk_feature_set
-#       fk_contratos_mutados
-#       previsoes_teste
-#       hash_codigo_gerador_modelo
-
-#   tabelas
-#     indice_part
-#     métricas avaliativas do experimento
-#     previsão producão
+readr::write_csv(avaliacao, 
+                 paste("data/metricas/metricas", "_",
+                       gsub(":", "", gsub("[[:space:]]", "_", Sys.Date())), ".csv", sep = ""))
 
 
 
 
+# ---------------------------
+# Parte VI:
+#   Salva experimento
+# ---------------------------
 
 
 
